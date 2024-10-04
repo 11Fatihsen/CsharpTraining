bbfa42160c8e09b2d5e01c347b8d0546ee79cf3b
11Fatihsen <fatihsen1104@gmail.com>
2024-10-04 13:49:19 +0300

    C# Training
    Murat Yücedağ C# Eğitimi

---
+++ CsharpEgitimKampi/01_MainSubjects/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            Console.WriteLine("Merhaba Dünya");
+            Console.Write("Hello World");
+            Console.WriteLine("SSS");
+            #endregion
+
+            #region String Değişkenler
+            //string 
+            //Değişken_türü değişken_adı;
+
+            string name;
+            name = "Murat";
+            Console.WriteLine(name);
+
+            string custumerName;
+            string custumerSurname;
+            string custumerPhone;
+            string custumerEmail, district, city;
+
+            custumerName = "Ali";
+            custumerSurname ="Çınar";
+            custumerPhone = "+90 500 400 30 20";
+            custumerEmail ="deneme@gmail.com";
+            district = "Üsküdar";
+            city = "İstanbul";
+
+            Console.WriteLine();
+            Console.WriteLine("**** Rezervasyon Kartı ****");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Müşteri: " + custumerName+ " "+ custumerSurname);
+            Console.WriteLine("İletişim: " + custumerPhone);
+            Console.WriteLine("Email Adresi: "+ custumerEmail);
+            Console.WriteLine("Adres: "+ district + "/" + city);
+            Console.WriteLine("---------------------------------------");
+            #endregion
+
+            #region İnt Değişkenler
+            int number = 11;
+            Console.WriteLine(number);
+
+            int hamburgerPrice = 300;
+            int friesPrice = 50;
+            int waterPrice = 15;
+
+   
+            Console.WriteLine();
+            Console.WriteLine("-----------**** Menü ****----------------");
+            Console.WriteLine("-----Hamburger: " + hamburgerPrice + "TL" );
+            Console.WriteLine("-----Patates: " + friesPrice + "TL");
+            Console.WriteLine("-----Su: " + waterPrice+"TL");
+            Console.WriteLine("---------------------------------------");
+
+            #endregion
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CsharpEgitimKampi/02_Variables/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Değişkenler
+            //double number;
+            //number= 0.15;
+            //Console.WriteLine(number);
+            double number1, number2;
+            number1 = 12.86;
+            number2 = 10;
+
+            double total1 = number1 + number2;
+            Console.WriteLine(total1);
+            #endregion
+            #region Char Değişkenler
+            char symbol = 'A';
+            Console.WriteLine(symbol);
+            #endregion
+            #region String Veri Girişi
+            //string passengerName, passengerSurname;
+            //Console.Write("Yolcu Adı: ");
+            //passengerName = Console.ReadLine();
+            //Console.Write("Yolcu soyadı: ");
+            //passengerSurname = Console.ReadLine();
+            //Console.WriteLine("Yolcu: " + passengerName +" "+ passengerSurname);
+            //Console.WriteLine();
+            #endregion
+            #region Int Veri Girişi
+            int FlightNumber;
+            Console.Write("Uçuş Numarasını giriniz: ");
+            FlightNumber=int.Parse(Console.ReadLine());
+            Console.WriteLine(FlightNumber);
+            #endregion
+            #region Double Veri Girişi
+            double bagWeight;
+            Console.Write("Bagaj ağırlığınızı giriniz: ");
+            bagWeight = double.Parse(Console.ReadLine());
+            Console.WriteLine(bagWeight);
+            #endregion
+            #region Karakter Veri Girişi
+            char gender;
+            Console.WriteLine("Cinsiyer Giriniz: ");
+            gender=char.Parse(Console.ReadLine());
+            Console.WriteLine(gender);
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
